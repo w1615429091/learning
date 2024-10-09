0ee73130028f10e061f5e73c72a81830b6712b9f
w1615429091 <166032564+w1615429091@users.noreply.github.com>
2024-10-09 08:26:53 +0800

    Add files via upload

--- mysql/10.08mysql.sql
+++ mysql/10.08mysql.sql
@@ -1,6 +1,7 @@
+drop database zs;
 create database zs;
 use zs;
-show database;
+show DATABASES;
 select database();
 
 create table t1(
@@ -46,16 +47,7 @@ insert into t1 values('t035','张35');
 
 
 select * from t1;
--- LIMIT 分页查询 LIMIT [跳过的个数],[跳过后提取的个数]
-SELECT * FROM t1 LIMIT 5,3;
-SELECT * FROM t1 LIMIT 10,99;
-SELECT * FROM t1 LIMIT 10,5;
-SELECT * FROM t1 LIMIT 20,10;
-SELECT * FROM t1 LIMIT 10;
-SELECT * FROM t1 LIMIT 10,10;
-SELECT * FROM t1 LIMIT 20,10;
-SELECT * FROM t1 LIMIT 30,10;
-SELECT * FROM t1 LIMIT 15,5;
+
 create table t2(
     tid varchar(4),
     tname varchar(8),
@@ -66,18 +58,13 @@ create table t2(
 
 insert into t2 values('t001','张三',18,'男','c001');
 insert into t2 values('t002','李四',28,'女','c001');
-insert into t2 values('t003','王五',25,'男','c002');
+insert into t2 values('t008','王9',25,'男','c001');
 insert into t2 values('t004','照片',31,'女','c002');
 insert into t2 values('t005','洪七',19,'女','c002');
-
+insert into t2 values('t006','洪8',29,'男','c002');
+insert into t2 values('t007','洪9',17,'女','c001');
 select * from t2;
-drop SELECT t2;
-SELECT sex,COUNT(*) 人数,SUM(age) 总年龄,AVG(age)平均年龄 FROM t2 GROUP BY sex;
-INSERT INTO t2 VALUES('t006','洪8','29','男','c002');
-INSERT INTO t2 VALUES('t007','洪9','17','女','c001');
-SELECT cid,sex,count(*) 总人数,AVG(age) 平均年龄 FROM t2 GROUP BY cid,sex HAVING AVG(age)>=20;
-DELETE FROM t2 WHERE tid='t001';
-SELECT cid,count(*) FROM t2 GROUP BY cid HAVING COUNT(*)<4;
+
 
 create table student(
     sid varchar(4),
@@ -87,6 +74,8 @@ create table student(
 insert into student values('s001','张三','c001');
 insert into student values('s002','李四','c001');
 insert into student values('s003','王五','c002');
+insert into student(sid,sname) values('s004','赵六');
+insert into student(sid,sname) values('s005','洪七');
 
 create table cls(
     cid varchar(4),
@@ -95,9 +84,47 @@ create table cls(
 
 insert into cls values('c001','飞天班');
 insert into cls values('c002','飞龙班');
+INSERT INTO cls VALUES('c003','飞猫班');
+INSERT INTO cls VALUES('c004','飞碟班');
+
+DELETE FROM cls WHERE cname='飞碟班';
 
 select * from student;
 select * from cls;
 
+SELECT * FROM student LEFT JOIN cls ON student.cid=cls.cid;
+SELECT * FROM student RIGHT JOIN cls ON student.cid=cls.cid;
+
 SELECT * FROM student,cls WHERE student.cid=cls.cid;
 
+SELECT
+	* 
+FROM
+	student
+	LEFT JOIN cls ON student.cid = cls.cid UNION
+SELECT
+	* 
+FROM
+	student
+	RIGHT JOIN cls ON student.cid = cls.cid;
+
+CREATE TABLE emp(
+eid VARCHAR(4),
+ename VARCHAR(8),
+mid VARCHAR(4)
+);
+DELETE FROM emp WHERE eid='e002';
+INSERT INTO emp VALUES('e001','张三','NULL');
+INSERT INTO emp VALUES('e002','张小一','e001');
+INSERT INTO emp VALUES('e003','张小二','e002');
+INSERT INTO emp VALUES('e004','张小三','e003');
+
+SELECT * FROM emp;
+
+SELECT * FROM emp a, emp b WHERE a.mid=b.eid;
+
+SELECT * FROM emp a LEFT JOIN emp b on a.mid = b.eid;
+
+SELECT a.eid,a.ename,a.mid,b.ename FROM emp a LEFT JOIN emp b on a.mid=b.eid;
+
+SELECT * FROM t2 WHERE cid IN (SELECT cid FROM t2 WHERE tname LIKE '王%');
