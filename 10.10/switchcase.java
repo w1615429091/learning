01b73a9ad556a6456c078ffae85d7e068b20c168
wsl <1615429091@qq.com>
2024-10-11 10:04:58 +0800

    2

--- 10.10/switchcase.java
+++ 10.10/switchcase.java
@@ -1,13 +1,19 @@
 public class switchcase {
     public static void main(String[] args) {
-        int money = 3;
+        int money = 1;
         switch (money) {
             case 1:System.out.println("矿泉水");
+            break;
             case 2:System.out.println("农夫山泉");
+            break;
             case 3:System.out.println("冰红茶");
+            break;
             case 4:System.out.println("阿萨姆");
+            break;
             case 5:System.out.println("茶π");
+            break;
             case 6:System.out.println("外星人电解质水");
+            break;
             default:System.out.println("没有喝水");
         }
     }
